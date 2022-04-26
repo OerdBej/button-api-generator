d6ce4711f0be70f1402a7ebebcfbde9c9f4e05ab
oerdbej <oerdbej@gmail.com>
2022-04-26 16:41:52 +0200

    this is random api generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  // http://api.quotable.io/random
+
+  useEffect(() => {
+    fetch("http://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((quote) => {
+        setQuote(quote.content);
+        setAuthor(quote.author);
+      });
+  }, []);
+
+  let fetchNewQuote = () => {
+    fetch("http://api.quotable.io/random")
+      .then((random) => random.json())
+      .then((quote) => {
+        setQuote(quote.content);
+        setAuthor(quote.author);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='quote'>
+        <h2>{quote}</h2>
+        <small>-{author}-</small>
+      </div>
+      <br />
+      <button className='btn' onClick={fetchNewQuote}>
+        This is button
+      </button>
     </div>
   );
 }
